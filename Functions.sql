99d1d1d4381048cd096756bd75505c110db0d287
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-06 21:03:26 +0530

    Add files via upload

---
+++ Functions.sql
@@ -0,0 +1,37 @@
+
+-- Find the number of employees
+SELECT COUNT(emp_id)
+FROM employee;
+
+-- Find the number of female emp before after 1970
+select COUNT(emp_id) 
+from employee
+where sex='F' and birth_date > '1971-01-01';
+
+-- Find the number of employees with super visors
+SELECT COUNT(super_id)
+FROM employee;
+
+-- Find the average of all employee's salaries
+SELECT AVG(salary)
+FROM employee;
+
+-- Find the sum of all employee's salaries
+SELECT SUM(salary)
+FROM employee;
+
+-- Find out how many males and females there are
+SELECT COUNT(sex), sex
+FROM employee
+GROUP BY sex;
+
+-- Find the total sales of each salesman
+SELECT SUM(total_sales), emp_id
+FROM works_with
+GROUP BY emp_id;
+
+-- Find the total amount of money spent by each client
+SELECT SUM(total_sales), client_id
+FROM works_with
+GROUP BY client_id;
+
